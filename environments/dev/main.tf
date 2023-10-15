51c5db5be8da18141a56ee488eab11c7d98a7b80
Leith <mhf.leith@gmail.com>
2023-10-15 18:54:52 +0100

    setting alb security group

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -18,3 +18,33 @@ module "vpc" {
     Environment = var.environment
   }
 }
+
+################################################################################
+# Security Groups
+################################################################################
+
+module "alb_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "${var.environment}-${var.id}-alb-sg"
+  description = "Security group for the ALB"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_rules = ["https-443-tcp", "http-80-tcp"]
+
+}
+
+module "asg_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "${var.environment}-${var.id}-asg-sg"
+  description = "Security group for the asg"
+  vpc_id      = module.vpc.vpc_id
+
+  computed_ingress_with_source_security_group_id = [
+    {
+      rule                     = "http-8080-tcp"
+      source_security_group_id = module.alb_sg.security_group_id
+    }
+  ]
+}   
