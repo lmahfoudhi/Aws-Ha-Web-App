c3313dc131c78c04f6ac0eb3455336bb218c3dea
Leith <mhf.leith@gmail.com>
2023-10-15 17:27:52 +0100

    define the backend to store the state

---
+++ environments/dev/backend/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "state_lock_bucket" {
+  bucket = "${var.environment}-${var.id}-s3-bucket"
+
+  tags = {
+    Name        = "${var.environment}-${var.id}-s3-bucket"
+    Environment = var.environment,
+    id          = var.id
+  }
+
+}
+
+resource "aws_dynamodb_table" "state_lock_table" {
+  name         = "${var.environment}-${var.id}-dynamodb-table"
+  billing_mode = var.billing_mode
+  hash_key     = var.hash_key
+
+  attribute {
+    name = var.attribute_name
+    type = var.attribute_type
+  }
+
+  tags = {
+    Name        = "${var.environment}-${var.id}-dynamodb-table"
+    Environment = var.environment,
+    id          = var.id
+  }
+
+
+}
---
+++ environments/dev/backend/variables.tf
@@ -0,0 +1,37 @@
+
+variable "region" {
+  type        = string
+  description = "Region of the alb-asg"
+}
+
+variable "billing_mode" {
+  type        = string
+  description = "Billing mode for dynamodb"
+}
+
+variable "hash_key" {
+  type        = string
+  description = "Hash key name of dynamodb"
+}
+
+variable "attribute_name" {
+  type        = string
+  description = "Attribute name of dynamodb"
+}
+
+variable "attribute_type" {
+  type        = string
+  description = "Attribute type of dynamodb"
+}
+
+
+variable "environment" {
+  type        = string
+  description = "The environment name for the resources."
+}
+
+
+variable "code" {
+  type        = string
+  description = "ID associated to the project"
+}
